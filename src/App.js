4fd37851601ee1520cddff01c1a21c8bf504cef8
ghedir <ghedirgahada77@gmail.com>
2021-06-17 22:48:39 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import AddTodo from "./components/AddTodo";
+import TodoList from "./components/TodoList";
+import { useDispatch, useSelector } from "react-redux";
+import { filterTodo } from "./redux/actions/todoActions";
 
 function App() {
+  const todos = useSelector((state) => state.tasks.todos);
+  const filter = useSelector((state) => state.filtre);
+  const dispatch = useDispatch();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="todo-app">
+      <h1>What's the Plan for Today?</h1>
+      <AddTodo />
+      <div className="btn-group" role="group">
+        <button
+          className="todo-button-all"
+          onClick={() => dispatch(filterTodo("All"))}
         >
-          Learn React
-        </a>
-      </header>
+          All
+        </button>
+        <button
+          className="todo-button-complete"
+          onClick={() => dispatch(filterTodo(true))}
+        >
+          Completed
+        </button>
+        <button
+          className="todo-button-incomple"
+          onClick={() => dispatch(filterTodo(false))}
+        >
+          Incomplete
+        </button>
+      </div>
+
+      <TodoList
+        todos={
+          filter === "All" ? todos : todos.filter((el) => el.isDone === filter)
+        }
+      />
     </div>
   );
 }
